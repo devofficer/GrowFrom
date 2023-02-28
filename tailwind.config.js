6c83880cdaf225c7e00355f387a4e62e77c09458
devofficer <myeyesontech@gmail.com>
2023-02-28 13:10:45 -0800

    init: inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./src/*.html"],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'journey': "url('/src/assets/img/journey.png')",
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
