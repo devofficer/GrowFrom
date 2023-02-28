e475d36ba967b64889df2961fb640c0b82770b08
devofficer <myeyesontech@gmail.com>
2023-02-28 13:20:36 -0800

    fix: update the image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
     content: ["./src/*.html"],
     theme: {
       extend: {
-        backgroundImage: {
-          'journey': "url('/src/assets/img/journey.png')",
-        },
       },
     },
     variants: {
